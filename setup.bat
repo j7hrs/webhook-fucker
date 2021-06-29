446c45a024aa335883b49a6979711792581be3ca
j7hrs <76898666+j7hrs@users.noreply.github.com>
2021-06-29 14:49:53 +0300

    Added setup file.
    Added setup file to install packages.

---
+++ setup.bat
@@ -0,0 +1,17 @@
+@echo off
+
+title Setup (0/2 packages installed)
+
+echo Installing packages...
+
+python -m pip install --upgrade pip
+
+title Setup (1/2 packages installed)
+
+pip install discord_webhook
+
+title Setup (Done!)
+
+echo Installed all packages!
+
+@pause
